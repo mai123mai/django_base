cef7a50b3d83f3c68b5fd179a951fe4b42ea8fba
mzh <1352743601@qq.com>
2022-09-26 14:37:00 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -13,12 +13,15 @@ from django.db import models
      M 就是选项
 '''
 
+
 class BookInfo(models.Model):
+    # 创建字段，字段类型...
     name = models.CharField(max_length=10)
+
 
 # 准备人物列表信息的模型类
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
     gender = models.BooleanField()
     # 外键约束：人物属于哪本书
-    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
