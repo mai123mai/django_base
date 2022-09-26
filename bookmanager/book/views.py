cfa78e9af1643cfd926a0f120151522c06e04774
mzh <1352743601@qq.com>
2022-09-26 17:02:19 +0800

    视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+'''
+视图函数：
+    所谓的视图函数，其实就是ｐｙｔｈｏｎ函数
+    视图函数有两个要求：
+        １．第一个参数是接收请求  就是 HttpRequest
+        
+
+'''
+# request
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+# 首页视图函数
+def index(request):
+    return HttpResponse('ok')
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from book.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path(路由,视图函数名)
+    # path('index/', index),
+    # path('blog/', include('book.urls')) # blog/index
+    # 　此时我们需要访问的话，需要添加　工程的path路由　＋　子应用的path路由
+    path('', include('book.urls')) # /index
 ]
